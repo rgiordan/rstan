63bb566e18b7bf1f6679594d24e9685ebd3f4e65
Ryan Giordano <rgiordan@gmail.com>
2018-04-23 21:12:28 -0700

    makevars and headers

--- install_StanHeaders.R
+++ install_StanHeaders.R
@@ -3,10 +3,10 @@
 # including its submodules. This requires the git2r and devtools packages
 
 path_rstan <- tempfile(pattern = "git2r-")
-git2r::clone("http://github.com/stan-dev/rstan", path_rstan, branch = "develop")
+git2r::clone("http://github.com/stan-dev/rstan", path_rstan, branch = "add_hessian3")
 git2r::clone("http://github.com/stan-dev/stan", 
              file.path(path_rstan, "StanHeaders", "inst", "include", "upstream"), 
-             branch = "develop") # may want to change this branch
+             branch = "add_hessian")
 git2r::clone("http://github.com/stan-dev/math", 
              file.path(path_rstan, "StanHeaders", "inst", "include", "mathlib"), 
              branch = "develop") # may want to change this branch
